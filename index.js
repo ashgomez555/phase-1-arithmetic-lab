fa049d0c4fb7ddcce314c7856f2fcd80f277c5d6
Ashley Gomez <ash.gomez555@gmail.com>
2021-11-22 18:19:30 -0700

    commit tree 5171e748ca8b543cefb926c048ffce6ac6e1061b

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31 
+let num2 = 2 
+
+let multiply= num1 * num2
+
+let num3 = 7
+let num4 = 3
+
+let mod = num3 - num4 
+
+
+const max = Math.max(5, 10, 15, 20)
+
+let random = Math.floor(Math.random()* 10 + 1)
